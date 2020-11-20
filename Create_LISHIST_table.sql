fd7fbab5ee80ac284262c6d44ab87269bdcbee64
Stephen McCready <stephenmccready@gmail.com>
2020-11-20 13:52:51 -0500

    Update and rename Create_LISHIST_table to Create_LISHIST_table.sql
    Added comments renamed to .sql

--- Create_LISHIST_table.sql
+++ Create_LISHIST_table.sql
@@ -1,9 +1,11 @@
+IF OBJECT_ID('[dbo].[tbl_LISHIST_Detail]') IS NOT NULL DROP TABLE [dbo].[tbl_LISHIST_Detail]
+
 CREATE TABLE [dbo].[tbl_LISHIST_Detail](
 	[LISHISTDetailId] [int] IDENTITY(1,1) NOT NULL,
 	[RecordType] [varchar](1) NULL,
 	[MCOContractNumber] [varchar](5) NULL,
 	[PBPNumber] [varchar](3) NULL,
-	[HICNumber] [varchar](12) NULL,
+	[BeneficiaryID] [varchar](12) NULL,
 	[Surname] [varchar](12) NULL,
 	[FirstName] [varchar](7) NULL,
 	[MiddleInitial] [varchar](1) NULL,
@@ -14,12 +16,28 @@ CREATE TABLE [dbo].[tbl_LISHIST_Detail](
 	[LIPSPercentage] [varchar](3) NULL,
 	[PremiumLISAmount] [varchar](8) NULL,
 	[LowIncomeCo-payLevelID] [varchar](1) NULL,
+		--	1 = High.
+		--	2 = Low.
+		--	3 = $0.
+		--	4 = 15%.
+		--	Co-pay level IDs 1 and 2 change each year.
 	[BeneficiarySourceofSubsidyCode] [varchar](1) NULL,
+		--	A = Determined Eligible for LIS by the SSA or a State Medicaid Agency.
+		--	D = Deemed Eligible for LIS.
 	[LISActivityFlag] [varchar](1) NULL,
+		--	N = No change in reported LIS data since last month’s data file.
+		--	Y = One of the following may have changed since the last month’s data file:
+			--	 Co-payment level
+			--	 Low-income premium subsidy level
+			--	 Low-income period start or end date
 	[PBPStartDate] [varchar](8) NULL,
 	[NetPartDPremiumAmount] [varchar](8) NULL,
 	[ContractYear] [varchar](4) NULL,
 	[InstitutionalStatusIndicator] [varchar](1) NULL,
+		--	1 = Institutionalized.
+		--	2 = Non-Institutionalized.
+		--	3 = Home and Community- Based Services (HCBS).
+		--	9 = Not applicable.
 	[PBPEnrollmentTerminationDate] [varchar](8) NULL,
 	[Filler01] [varchar](56) NULL,
 	[DataImported] [datetime] NOT NULL,
